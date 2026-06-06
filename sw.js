52522e89747262867eed9aa697091b5fcac60346
Fan-Lin <150505581+Fan-Lin@users.noreply.github.com>
2026-06-06 09:04:22 -0400

    update to v0.2.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'withme-v0.2.1';
+const CACHE_NAME = 'withme-v0.2.2';
 const ASSETS = [
   '/withme/',
   '/withme/index.html',
@@ -18,9 +18,8 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
